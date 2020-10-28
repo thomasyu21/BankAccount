0ff5949d56acc104dccd10b6fa9a743c6feb6cec
thomasyu21 <thomasyu085@gmail.com>
2020-10-27 22:50:45 -0400

    Created getBalance() method to print the balance in the account. Fixed constructor.

--- BankAccount.java
+++ BankAccount.java
@@ -1,14 +1,17 @@
-public Class BankAccount{
+public class BankAccount{
 
   private double balance;
   private int accountID;
   private String password;
 
-  public BankAccount(double b, int a, String p){
-    balance = 0;
+  public BankAccount(int a, String p){
+    balance = 0.0;
     accountID = a;
     password = p;
   }
 
+  public String getBalance() {
+    return ("Balance is " + balance);
+  }
 
 }
--- BankAccountTester.java
+++ BankAccountTester.java
@@ -1,6 +1,7 @@
 public class BankAccountTester{
 
   public static void main(String[] args) {
-    
+    BankAccount A1 = new BankAccount(0000,"password1");
+    System.out.println(A1.getBalance());
   }
 }
